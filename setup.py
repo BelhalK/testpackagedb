5919d1c57bd2b9079dee389d9809556fad5cc61a
BelhalK <belhal.karimi@gmail.com>
2023-03-18 11:13:01 -0700

    maj(

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='testpackagedb',
+        version='0.1',
+        description='custom business functions',
+        url='',
+        author='Belhal Karimi',
+        author_email='belhal.karimi@nike.com',
+        license='MIT',
+        packages=['testpackagedb'])
---
+++ testpackagedb/functions.py
@@ -0,0 +1,19 @@
+def AddNumbers(i, j):
+    return i + j
+
+def SubtractNumbers(i, j):
+    return i - j
+
+def MultiplyNumbers(i, j):
+    return i * j
+
+
+def loadmodel(modelname='search_relevance_v0')
+	bimodel_finetuned = "s3://nikeplus-ngap-test/bernardabayowa/{}/models/2023-02-03/".format(modelnam)
+	local_bimodel_path='/tmp/bimodel_finetuned/'
+
+	bimodel_finetuned_cmd="aws s3 cp {} {} --acl bucket-owner-full-control --recursive".format(bimodel_finetuned, local_bimodel_path)
+	os.environ['bimodel_finetuned_cmd']=bimodel_finetuned_cmd
+
+	dbutils.fs.mkdirs("file:"+local_bimodel_path)
+return local_bimodel_path
