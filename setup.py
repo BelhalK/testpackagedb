4d4319a2b4e6b8d4210e5aea121d883be976ae66
BelhalK <belhal.karimi@gmail.com>
2023-03-18 11:35:19 -0700

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='testpackagedb',
-        version='0.2',
+        version='0.3',
         description='custom business functions',
         url='',
         author='Belhal Karimi',
